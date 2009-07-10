18997973e801a13673a35a7f2ea1e6e0dd68dd1d
Chris Massey <Chris.Massey@usace.army.mil>
2009-07-10 18:50:38 +0000

    Implemented bug fix for global elevation outputs from fastfloodplain.
    git-svn-id: https://adcirc.renci.org/svn/adcirc48/trunk@215 c8170193-e745-4495-a2da-fcbf5ee5578e

--- work/cmplrflags.mk
+++ work/cmplrflags.mk
@@ -9,15 +9,16 @@ ifeq ($(MACHINE)-$(OS),x86_64-linux-gnu)
 # ***NOTE*** User must select between various Linux setups
 #            by commenting/uncommenting the appropriate compiler
 #
-#compiler=gnu
+compiler=gnu
+#compiler=g95
 #compiler=intel
 #compiler=intel-lonestar
 #compiler=cray_xt3
 #compiler=pgi
-compiler=pgi-ranger
+#compiler=pgi-ranger
 #
 # 
-ifeq ($(compiler),gnu)
+ifeq ($(compiler),g95)
   PPFC		:=  g95
   FC		:=  g95
   PFC		:=  mpif90
@@ -42,6 +43,31 @@ ifeq ($(compiler),gnu)
   endif
 endif
 #
+# Added compiler flags for gfortran and gcc on 64-bit Linux machine
+ifeq ($(compiler),gnu)
+  PPFC		:=  gfortran
+  FC		:=  gfortran
+  PFC		:=  mpif90
+  FFLAGS1	:=  $(INCDIRS) -O2 -mcmodel=medium -ffixed-line-length-132 -march=k8 -m64
+  FFLAGS2	:=  $(FFLAGS1)
+  FFLAGS3	:=  $(FFLAGS1)
+  DA		:=  -DREAL8 -DLINUX -DCSCA
+  DP		:=  -DREAL8 -DLINUX -DCSCA -DCMPI
+  DPRE		:=  -DREAL8 -DLINUX
+  IMODS 	:=  -I
+  CC		:= gcc
+  CCBE		:= $(CC)
+  CFLAGS	:= $(INCDIRS) -O2 -mcmodel=medium -DLINUX -march=k8 -m64
+  CLIBS	:= 
+  LIBS		:=  
+  MSGLIBS	:=  
+  $(warning (INFO) Corresponding compilers and flags found in cmplrflags.mk.)
+  ifneq ($(FOUND),TRUE)
+     FOUND := TRUE
+  else 
+     MULTIPLE := TRUE
+  endif
+endif
 # jgf45.12 These flags work on the UNC Topsail Cluster.
 ifeq ($(compiler),intel)
   PPFC            :=  ifort	
