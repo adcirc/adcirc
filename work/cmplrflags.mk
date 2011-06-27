b3edd6d3d7367ba42b0ec76fa1fd62e81fa0b819
Jason Fleming <jason.fleming@seahorsecoastal.com>
2011-06-27 20:22:37 +0000

    v50.23: Added NWS=319 to list of allowable NWS values in read_input.F; fixed initialization in hstime.F; fixed debug flags for intel compiler.
    git-svn-id: https://adcirc.renci.org/svn/adcirc/trunk@679 c8170193-e745-4495-a2da-fcbf5ee5578e

--- work/cmplrflags.mk
+++ work/cmplrflags.mk
@@ -9,7 +9,7 @@ ifeq ($(MACHINE)-$(OS),x86_64-linux-gnu)
 # ***NOTE*** User must select between various Linux setups
 #            by commenting/uncommenting the appropriate compiler
 #
-compiler=gnu
+#compiler=gnu
 #compiler=g95
 #compiler=intel
 #compiler=intel-lonestar
@@ -79,8 +79,7 @@ ifeq ($(compiler),intel)
   PFC           :=  mpif90
   FFLAGS1       :=  $(INCDIRS) -O2 -FI  -Vaxlib -assume byterecl -132
   ifeq ($(DEBUG),full)
-     FFLAGS1       :=  $(INCDIRS) -g -O0 -traceback -check all -FI -Vaxlib -assume byterecl -132 -DNETCDF_TRACE -DHOTSTART_TRACE -DWRITE_OUTPUT_TRACE -DGLOBAL_TRACE
-#     FFLAGS1       :=  $(INCDIRS) -g -O0 -traceback -check all -FI -Vaxlib -assume byterecl -132 -DALL_TRACE -DFLUSH_MESSAGES -DFULL_STACK  #-DNETCDF_TRACE -DHOTSTART_TRACE -DWRITE_OUTPUT_TRACE -DGLOBAL_TRACE
+     FFLAGS1       :=  $(INCDIRS) -g -O0 -traceback -check all -FI -Vaxlib -assume byterecl -132 -DALL_TRACE -DFULL_STACK -DFLUSH_MESSAGES
   endif
   FFLAGS2       :=  $(FFLAGS1)
   FFLAGS3       :=  $(FFLAGS1)
