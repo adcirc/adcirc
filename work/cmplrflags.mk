d47edaeb3ca4193e7fda64b550b3e7953aa79eed
Jason Fleming <jason.fleming@seahorsecoastal.com>
2014-10-31 14:23:29 +0000

    v51.33.06: Fixes to enable compilation with intel14 compilers on hatteras at RENCI.
    git-svn-id: https://adcirc.renci.org/svn/adcirc/branches/GAHM_jie@1036 c8170193-e745-4495-a2da-fcbf5ee5578e

--- work/cmplrflags.mk
+++ work/cmplrflags.mk
@@ -97,7 +97,7 @@ ifeq ($(compiler),intel)
   PPFC            :=  ifort
   FC            :=  ifort
   PFC           :=  mpif90
-  FFLAGS1       :=  $(INCDIRS) -O3 -FI -assume byterecl -132 -i-dynamic -xSSE4.2 -assume buffered_io
+  FFLAGS1       :=  $(INCDIRS) -O2 -FI -assume byterecl -132 -i-dynamic -xSSE4.2 -assume buffered_io
   ifeq ($(DEBUG),full)
      FFLAGS1       :=  $(INCDIRS) -g -O0 -traceback -debug -check all -i-dynamic -FI -assume byterecl -132 -DALL_TRACE -DFULL_STACK -DFLUSH_MESSAGES
   endif
@@ -112,7 +112,7 @@ ifeq ($(compiler),intel)
   IMODS         :=  -I
   CC            := icc
   CCBE		:= $(CC)
-  CFLAGS        := $(INCDIRS) -O3 -xSSE4.2 -m64 -mcmodel=medium -DLINUX
+  CFLAGS        := $(INCDIRS) -O2 -xSSE4.2 -m64 -mcmodel=medium -DLINUX
   ifeq ($(DEBUG),full)
      CFLAGS        := $(INCDIRS) -g -O0 -march=k8 -m64 -mcmodel=medium -DLINUX
   endif
@@ -131,6 +131,16 @@ ifeq ($(compiler),intel)
         #FLIBS          :=$(FLIBS) -L/usr/lib64 -lnetcdff
 #        NETCDFHOME  :=/shared/apps/RHEL-5/x86_64/NetCDF/netcdf-4.1.1-gcc4.1-ifort
         NETCDFHOME  :=/shared/apps/RHEL-5/x86_64/NetCDF/netcdf-4.1.2-gcc4.1-ifort
+     endif
+     ifeq ($(MACHINENAME),hatteras)
+        #FLIBS       := $(FLIBS) -L/projects/ncfs/apps/croatan/netcdf/lib -lnetcdff -lnetcdf
+        #NETCDFHOME    :=/projects/ncfs/apps/croatan/netcdf
+        #FFLAGS1       :=$(FFLAGS1) -I/projects/ncfs/apps/croatan/netcdf/include
+        FLIBS       := $(FLIBS) -L/usr/share/Modules/software/RHEL-6.5/netcdf/netcdf-4.1.3_intel-14.0.3/lib -lnetcdff -lnetcdf
+        NETCDFHOME    :=/usr/share/Modules/software/RHEL-6.5/netcdf/netcdf-4.1.3_intel-14.0.3
+        FFLAGS1       :=$(FFLAGS1) -I/usr/share/Modules/software/RHEL-6.5/netcdf/netcdf-4.1.3_intel-14.0.3/include
+        FFLAGS2       :=$(FFLAGS1)
+        FFLAGS3       :=$(FFLAGS1)
      endif
      ifeq ($(MACHINENAME),killdevil)
         HDF5HOME       :=/nas02/apps/hdf5-1.8.5/lib
