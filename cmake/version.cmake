bdaa3e97be75627bded37e6c51923b2204c74003
Zach <zachary.cobell@arcadis-us.com>
2017-01-27 10:42:12 -0700

    Handling version_cmake.F file when conducting Windows Native build
    The windows native build will assume that it does not have to
    generate the version file itself. Instead, it will fall back
    to a version file made available in the main source directory.
    
    By default, it looks for version.F, however if not found it will
    fall back to version_default.F.

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,8 +1,21 @@
 
-ADD_LIBRARY(version ${CMAKE_SOURCE_DIR}/version.F)
-ADD_CUSTOM_COMMAND( OUTPUT ${CMAKE_SOURCE_DIR}/version.F
-    COMMAND ./adcircVersion.sh ${CMAKE_SOURCE_DIR}
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/work/scripts
-    COMMENT "Generating ADCIRC version...")
+ADD_LIBRARY(version ${CMAKE_BINARY_DIR}/CMakeFiles/version_cmake.F)
+IF(WIN32)
+    IF(EXISTS ${CMAKE_SOURCE_DIR}/version.F)
+        ADD_CUSTOM_COMMAND( OUTPUT ${CMAKE_BINARY_DIR}/CMakeFiles/version_cmake.F 
+            COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/version.F ${CMAKE_BINARY_DIR}/CMakeFiles/version_cmake.F
+            COMMENT "Generating ADCIRC version...")
+    ELSEIF(EXISTS ${CMAKE_SOURCE_DIR}/version_default.F)
+        ADD_CUSTOM_COMMAND( OUTPUT ${CMAKE_BINARY_DIR}/CMakeFiles/version_cmake.F
+            COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/version_default.F ${CMAKE_BINARY_DIR}/CMakeFiles/version_cmake.F
+            COMMENT "Generating ADCIRC version...")
+    ENDIF(EXISTS ${CMAKE_SOURCE_DIR}/version.F)
+ELSE(WIN32)
+    ADD_CUSTOM_COMMAND( OUTPUT ${CMAKE_BINARY_DIR}/CMakeFiles/version_cmake.F
+        COMMAND ./adcircVersion.sh ${CMAKE_SOURCE_DIR} >/dev/null
+        COMMAND cp ${CMAKE_SOURCE_DIR}/version.F ${CMAKE_BINARY_DIR}/CMakeFiles/version_cmake.F
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/work/scripts
+        COMMENT "Generating ADCIRC version...")
+ENDIF(WIN32)
 SET_TARGET_PROPERTIES(version PROPERTIES Fortran_MODULE_DIRECTORY CMakeFiles/version_mod)
 SET_TARGET_PROPERTIES(version PROPERTIES COMPILE_FLAGS "${Fortran_LINELENGTH_FLAG} ${Fortran_COMPILER_SPECIFIC_FLAG}") 
