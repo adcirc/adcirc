f7faa7537158582cecc483474c976e50fb888240
Zach Cobell <zcobell@users.noreply.github.com>
2019-09-08 18:41:57 -0600

    V53merge to master (#188)
    v53merge to master

--- util/adcircResultsComparison.F90
+++ util/adcircResultsComparison.F90
@@ -8,7 +8,13 @@
 ! solution is different between them. The solution can be compared
 ! across formats as well. A return code of 0 is returned if the files
 ! are considered to match. A return code of 1 is returned otherwise.
+!-----------------------------------------------------------------------
+! @jasonfleming 20181129: Sample command line for compiling this program:
+! 
+! gfortran -o adcircResultsComparison -I/usr/include -L/usr/lib adcircResultsComparison.F90 -lnetcdf -lnetcdff
 !
+! RENCI hatteras:
+! ifort -o adcircResultsComparison -I/usr/share/Modules/software/CentOS-7/netcdf-Fortran/4.4.0_intel-18.0.0/include -L/usr/share/Modules/software/CentOS-7/netcdf-Fortran/4.4.0_intel-18.0.0/lib adcircResultsComparison.F90 -lnetcdf -lnetcdff
 !-----------------------------------------------------------------------
 
         MODULE adcircCompare_module
