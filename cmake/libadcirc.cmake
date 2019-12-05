c8211888d06e9c1d3dcb651ae675c36ccb150173
WPringle <wpringle@nd.edu>
2019-12-05 17:11:59 -0500

    adding my files to libadcirc

--- cmake/libadcirc.cmake
+++ cmake/libadcirc.cmake
@@ -1,4 +1,4 @@
-SET( LIBADC_SOURCES   src/sizes.F KDTREE2/kdtree2.F src/sponge_layer.F 
+SET( LIBADC_SOURCES   src/sizes.F KDTREE2/kdtree2.F
                       src/global.F src/boundaries.F src/global_3dvs.F
                       src/messenger.F src/mesh.F src/harm.F wind/vortex.F 
                       src/wind.F src/hashtable.F src/owiwind.F src/rs2.F src/owi_ice.F 
@@ -7,7 +7,8 @@ SET( LIBADC_SOURCES   src/sizes.F KDTREE2/kdtree2.F src/sponge_layer.F
                       src/netcdfio.F src/control.F src/xdmfio.F src/writer.F 
                       src/write_output.F src/couple2swan.F src/adcirc.F
                       src/weir_boundary.F src/read_input.F src/cstart.F src/hstart.F 
-                      src/timestep.F src/vsmy.F src/transport.F )
+                      src/timestep.F src/vsmy.F src/transport.F
+                      src/sponge_layer.F src/quadtrature.F src/couple2baroclinic3D.F)
 
 IF(BUILD_LIBADCIRC_STATIC)
 
