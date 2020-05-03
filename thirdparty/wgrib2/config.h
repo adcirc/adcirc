b267457e83563b95ada8f23cb67edfffdabffe54
Zach Cobell <zcobell@gmail.com>
2020-05-02 20:55:04 -0600

    Adding build of the wgrib and datetime libraries to adcirc
    CMake will build these libraries automatically from the thirdparty
    folder

---
+++ thirdparty/wgrib2/config.h
@@ -0,0 +1,20 @@
+/* config.h */
+#define USE_REGEX
+/* #undef USE_TIGGE */
+/* #undef DISABLE_ALARM */
+#define DISABLE_TIMEZONE
+/* #undef USE_UDF */
+#define IPOLATES_LIB "ip2lib_d"
+#define USE_IPOLATES 3
+/* #undef USE_G2CLIB */
+/* #undef USE_PROJ4 */
+/* #undef USE_JASPER */
+/* #undef USE_AEC */
+/* #undef USE_NETCDF3 */
+#define USE_NETCDF4
+/* #undef USE_MYSQL */
+#define CC "adcirc_cc"
+#define FORTRAN "adcirc_ftn"
+#define BUILD_COMMENTS "adcirc build"
+/* #undef USE_PNG */
+/* #undef WMO_VALIDATION */
