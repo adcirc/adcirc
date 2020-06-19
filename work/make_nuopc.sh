c504a60ef7ff37a59dd95f3c8891f560f941d214
Damrongsak Wirasaet <dwirasae@athos.crc.nd.edu>
2020-06-19 14:58:59 -0400

    Add NUOPCE NEM CAP

---
+++ work/make_nuopc.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+## moghimis@gmail.com - 01/31/2020
+## This script compiles ADCIRC model with a pre-selected sets of
+## functionalities in NUOPC coupled mode in Hera computer.
+# To run: ./make_nuopc esmf-impi-env.sh intel
+
+esmf_env=$1
+comp_opt=$2
+compiler=intel
+
+# environ required by ESMF/NUOPC
+if [[ ! -z $esmf_env ]]; then
+    echo "$0: Sourcing environ file $esmf_env"
+    source $esmf_env
+elif [[ ! -z $ESMFMKFILE ]]; then
+    echo "$0: Setting environ for ESMFMKFILE"
+else
+    echo "$0: ESMFMKFILE enviorn is not set, can not continue!"
+    exit 1
+fi
+ 
+
+if [[ ! -z $comp_opt ]]; then
+    echo "$0: Setting compiler option to $comp_opt"
+    export compiler=$comp_opt
+else
+    echo "$0: Setting compiler option by default to intel"
+    export compiler=intel
+fi
+
+export NETCDFLAG=enable
+export NETCDF4FLAG=enable
+export NETCDF4_COMPRESSION=enable
+
+export ADCDIR=$(pwd)
+
+# #make padcirc -d compiler=$compiler NETCDFLAG=enable NETCDF4FLAG=enable NETCDF4_COMPRESSION=enable
+export MAKELEVEL=0
+make NETCDF=enable NETCDF4=enable libadc.a 
+
+# build adcric nuopc
+cd  ../cpl/nuopc
+make -f makefile.adc_cap.nuopc nuopc
+
+cd $ADCDIR
+make NETCDF=enable NETCDF4=enable adcprep
+
+#
+#build tide_fac exe
+mkdir -p $ADCDIR/util
+cd $ADCDIR/../util/estofs_tide_fac/
+make
+cp -f estofs_tide_fac $ADCDIR/util/tidefac
+
+
+
+
+
+
+
+
+
+
+
