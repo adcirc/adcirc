057a723236ba4d1dd9ade54074c02a14bca8a9ce
pvelissariou1 <panagiotis.velissariou@noaa.gov>
2022-02-23 10:27:59 -0600

    added GSW-Fortran and OC-GTSM in thirdparty

---
+++ thirdparty/GSW-Fortran/cmake/compiler_flags.cmake
@@ -0,0 +1,7 @@
+if( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
+    include( compiler_flags_GNU_Fortran )
+elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" AND NOT WIN32 )
+    include( compiler_flags_Intel_Fortran )
+else()
+    message( STATUS "Fortran compiler with ID ${CMAKE_Fortran_COMPILER_ID} will be used with CMake default options")
+endif()
