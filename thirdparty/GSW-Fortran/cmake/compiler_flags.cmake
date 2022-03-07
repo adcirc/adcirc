8ae220af940cdac80d2a0b7ad865d7a0342c4f44
pvelissariou1 <panagiotis.velissariou@noaa.gov>
2022-03-06 23:13:40 -0600

    removed GSW-Fortran & OC-GTSM from thirdparty; they don't belong here

--- thirdparty/GSW-Fortran/cmake/compiler_flags.cmake
+++
@@ -1,7 +0,0 @@
-if( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
-    include( compiler_flags_GNU_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" AND NOT WIN32 )
-    include( compiler_flags_Intel_Fortran )
-else()
-    message( STATUS "Fortran compiler with ID ${CMAKE_Fortran_COMPILER_ID} will be used with CMake default options")
-endif()
