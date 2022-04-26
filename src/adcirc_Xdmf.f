a8c348f3575336cd22c0d2e6219ff0990d2779ee
Panagiotis Velissariou <panagiotis.velissariou@noaa.gov>
2022-04-25 23:25:25 -0500

    added missing code blocks form adcirc-cg/development; miscellaneous code and format fixes

--- src/adcirc_Xdmf.f
+++ src/adcirc_Xdmf.f
@@ -8,7 +8,7 @@
       integer xdmfstoremap
       integer xdmfaddcoordinate
       integer xdmfaddset
-      
+
       ! Array Type
       integer XDMF_ARRAY_TYPE_INT8
       integer XDMF_ARRAY_TYPE_INT16
@@ -19,14 +19,14 @@
       integer XDMF_ARRAY_TYPE_UINT32
       integer XDMF_ARRAY_TYPE_FLOAT32
       integer XDMF_ARRAY_TYPE_FLOAT64
-      
+
       ! Attribute Center
       integer XDMF_ATTRIBUTE_CENTER_GRID
       integer XDMF_ATTRIBUTE_CENTER_CELL
       integer XDMF_ATTRIBUTE_CENTER_FACE
       integer XDMF_ATTRIBUTE_CENTER_EDGE
       integer XDMF_ATTRIBUTE_CENTER_NODE
-      
+
       ! Attribute Type
       integer XDMF_ATTRIBUTE_TYPE_SCALAR
       integer XDMF_ATTRIBUTE_TYPE_VECTOR
@@ -35,15 +35,15 @@
       integer XDMF_ATTRIBUTE_TYPE_TENSOR6
       integer XDMF_ATTRIBUTE_TYPE_GLOBALID
       integer XDMF_ATTRIBUTE_TYPE_NOTYPE
-      
+
       ! Geometry Type
       integer XDMF_GEOMETRY_TYPE_XYZ
       integer XDMF_GEOMETRY_TYPE_XY
-      
+
       ! Grid Collection Type
       integer XDMF_GRID_COLLECTION_TYPE_SPATIAL
       integer XDMF_GRID_COLLECTION_TYPE_TEMPORAL
-      
+
       ! Topology Type
       integer XDMF_TOPOLOGY_TYPE_POLYVERTEX
       integer XDMF_TOPOLOGY_TYPE_POLYLINE
@@ -74,21 +74,21 @@
       integer XDMF_TOPOLOGY_TYPE_HEXAHEDRON_1000
       integer XDMF_TOPOLOGY_TYPE_HEXAHEDRON_1331
       integer XDMF_TOPOLOGY_TYPE_MIXED
-      
+
       ! Set Type
       integer XDMF_SET_TYPE_NODE
       integer XDMF_SET_TYPE_CELL
       integer XDMF_SET_TYPE_FACE
       integer XDMF_SET_TYPE_EDGE
-      
+
       ! Grid Type
       integer XDMF_GRID_TYPE_CURVILINEAR
       integer XDMF_GRID_TYPE_RECTILINEAR
       integer XDMF_GRID_TYPE_REGULAR
       integer XDMF_GRID_TYPE_UNSTRUCTURED
-      
+
       !------------------------------------------------------
-      
+
       parameter (XDMF_ARRAY_TYPE_INT8    = 0)
       parameter (XDMF_ARRAY_TYPE_INT16   = 1)
       parameter (XDMF_ARRAY_TYPE_INT32   = 2)
@@ -98,13 +98,13 @@
       parameter (XDMF_ARRAY_TYPE_UINT32  = 6)
       parameter (XDMF_ARRAY_TYPE_FLOAT32 = 7)
       parameter (XDMF_ARRAY_TYPE_FLOAT64 = 8)
-      
+
       parameter (XDMF_ATTRIBUTE_CENTER_GRID = 100)
       parameter (XDMF_ATTRIBUTE_CENTER_CELL = 101)
       parameter (XDMF_ATTRIBUTE_CENTER_FACE = 102)
       parameter (XDMF_ATTRIBUTE_CENTER_EDGE = 103)
       parameter (XDMF_ATTRIBUTE_CENTER_NODE = 104)
-      
+
       parameter (XDMF_ATTRIBUTE_TYPE_SCALAR   = 200)
       parameter (XDMF_ATTRIBUTE_TYPE_VECTOR   = 201)
       parameter (XDMF_ATTRIBUTE_TYPE_TENSOR   = 202)
@@ -112,13 +112,13 @@
       parameter (XDMF_ATTRIBUTE_TYPE_TENSOR6  = 204)
       parameter (XDMF_ATTRIBUTE_TYPE_GLOBALID = 205)
       parameter (XDMF_ATTRIBUTE_TYPE_NOTYPE   = 206)
-      
+
       parameter (XDMF_GEOMETRY_TYPE_XYZ  = 301)
       parameter (XDMF_GEOMETRY_TYPE_XY   = 302)
-      
+
       parameter (XDMF_GRID_COLLECTION_TYPE_SPATIAL  = 400)
       parameter (XDMF_GRID_COLLECTION_TYPE_TEMPORAL = 401)
-      
+
       parameter (XDMF_TOPOLOGY_TYPE_POLYVERTEX       = 500)
       parameter (XDMF_TOPOLOGY_TYPE_POLYLINE         = 501)
       parameter (XDMF_TOPOLOGY_TYPE_POLYGON          = 502)
@@ -148,14 +148,14 @@
       parameter (XDMF_TOPOLOGY_TYPE_HEXAHEDRON_1000  = 526)
       parameter (XDMF_TOPOLOGY_TYPE_HEXAHEDRON_1331  = 527)
       parameter (XDMF_TOPOLOGY_TYPE_MIXED            = 528)
-      
+
       parameter (XDMF_SET_TYPE_NODE = 601)
       parameter (XDMF_SET_TYPE_CELL = 602)
       parameter (XDMF_SET_TYPE_FACE = 603)
       parameter (XDMF_SET_TYPE_EDGE = 604)
-      
+
       parameter (XDMF_GRID_TYPE_CURVILINEAR	= 701)
       parameter (XDMF_GRID_TYPE_RECTILINEAR	= 702)
       parameter (XDMF_GRID_TYPE_REGULAR	= 703)
       parameter (XDMF_GRID_TYPE_UNSTRUCTURED	= 704)
-      
+
