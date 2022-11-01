64c195a8c557b4e193a6086b0773fb758872c994
cblakely <cblakely@nd.edu>
2022-11-01 19:00:34 -0400

    Adds ITFric as forcing, Upgrades to OGCM coupling
    Adds src/internaltide.F to cmake files
    Adds src/internaltide.F to libadcirc.cmake
    Adds itFricControl namelist to prep.F, presizes.F, read_global.F
    Adds support for baroclinic values pre-computed on ADCIRC grid to
    couple2baroclinic3D.F (IDEN = 7, 8)
    Changes size of TKM matrix in global.F from 3xNP to 5xNP for use in
    resynthesizing internal tide dissipation
    Adds internal tide generation forcing to gwce.F
    Adds internal tide generation forcing to momentum.F
    Adds src/internaltide.F which resynthesizes tidal velocities from a
    harmonic decomposition.
    Fixes a bug in maptosubdomainRealMPI and maptosubdomainIntMPI in
    messenger.F
    Adds support for itFricControl namelist in apply2dinternalwavedrag in
    nodalattr.F
    Adds itFricControl namelist to read_input.F
    Adds support for baroclinic values pre-computed on ADCIRC grid to
    read_input.F (adds IDEN = 7, 8 cases from densityControl namelist)
    Adds support for internal tide generation forcing to timestep.F

--- cmake/adcswan.cmake
+++ cmake/adcswan.cmake
@@ -75,37 +75,37 @@ if(BUILD_ADCSWAN AND PERL_FOUND)
       ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/swan_serial_source/SwanGradVel.f90)
 
   set(ADCSWAN1_SOURCES
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/sizes.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/constants.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/KDTREE2/kdtree2.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/global.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/boundaries.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/mesh.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/vew1d.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/hashtable.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/global_3dvs.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/harm.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/wind/vortex.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/wind.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/rs2.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/itpackv.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/nodalattr.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/globalio.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/netcdfio.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/subdomain.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/gwce.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/wetdry.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/momentum.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/control.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/xdmfio.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/write_output.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/couple2swan.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/sponge_layer.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/quadrature.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/couple2baroclinic3D.F)
+      ${CMAKE_SOURCE_DIR}/src/sizes.F
+      ${CMAKE_SOURCE_DIR}/thirdparty/KDTREE2/kdtree2.F
+      ${CMAKE_SOURCE_DIR}/src/global.F
+      ${CMAKE_SOURCE_DIR}/src/boundaries.F
+      ${CMAKE_SOURCE_DIR}/src/mesh.F
+      ${CMAKE_SOURCE_DIR}/src/vew1d.F
+      ${CMAKE_SOURCE_DIR}/src/hashtable.F
+      ${CMAKE_SOURCE_DIR}/src/global_3dvs.F
+      ${CMAKE_SOURCE_DIR}/src/harm.F
+      ${CMAKE_SOURCE_DIR}/wind/vortex.F
+      ${CMAKE_SOURCE_DIR}/src/wind.F
+      ${CMAKE_SOURCE_DIR}/src/owiwind.F
+      ${CMAKE_SOURCE_DIR}/src/owiwind_netcdf.F
+      ${CMAKE_SOURCE_DIR}/src/rs2.F
+      ${CMAKE_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_SOURCE_DIR}/src/itpackv.F
+      ${CMAKE_SOURCE_DIR}/src/nodalattr.F
+      ${CMAKE_SOURCE_DIR}/src/globalio.F
+      ${CMAKE_SOURCE_DIR}/src/netcdfio.F
+      ${CMAKE_SOURCE_DIR}/src/subdomain.F
+      ${CMAKE_SOURCE_DIR}/src/gwce.F
+      ${CMAKE_SOURCE_DIR}/src/wetdry.F
+      ${CMAKE_SOURCE_DIR}/src/momentum.F
+      ${CMAKE_SOURCE_DIR}/src/control.F
+      ${CMAKE_SOURCE_DIR}/src/xdmfio.F
+      ${CMAKE_SOURCE_DIR}/src/write_output.F
+      ${CMAKE_SOURCE_DIR}/src/couple2swan.F
+      ${CMAKE_SOURCE_DIR}/src/sponge_layer.F
+      ${CMAKE_SOURCE_DIR}/src/quadrature.F
+      ${CMAKE_SOURCE_DIR}/src/couple2baroclinic3D.F
+      ${CMAKE_SOURCE_DIR}/src/internaltide.F)
 
   set(ADCSWAN_SOURCES
       ${CMAKE_CURRENT_SOURCE_DIR}/src/adcirc.F
