mtcontre/adcirc-Hydrology

Adding hydrology (precipitation and source nodes river connection) and variable Manning values as nodal attribute

cmake/adcirc.cmake
@@ -14,6 +14,7 @@ if(BUILD_ADCIRC)
       ${CMAKE_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_SOURCE_DIR}/src/rs2.F
       ${CMAKE_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_SOURCE_DIR}/src/itpackv.F
       ${CMAKE_SOURCE_DIR}/src/nodalattr.F
       ${CMAKE_SOURCE_DIR}/src/globalio.F

cmake/adcprep.cmake
@@ -13,6 +13,7 @@ if(BUILD_ADCPREP)
       ${CMAKE_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_SOURCE_DIR}/src/rs2.F
       ${CMAKE_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_SOURCE_DIR}/src/wind.F
       ${CMAKE_SOURCE_DIR}/prep/presizes.F
       ${CMAKE_SOURCE_DIR}/prep/pre_global.F

cmake/adcswan.cmake
@@ -89,6 +89,7 @@ if(BUILD_ADCSWAN AND PERL_FOUND)
       ${CMAKE_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_SOURCE_DIR}/src/rs2.F
       ${CMAKE_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_SOURCE_DIR}/src/itpackv.F
       ${CMAKE_SOURCE_DIR}/src/nodalattr.F
       ${CMAKE_SOURCE_DIR}/src/globalio.F

cmake/libadcirc.cmake
@@ -14,6 +14,7 @@ set(LIBADC_SOURCES
     ${CMAKE_SOURCE_DIR}/src/owiwind_netcdf.F
     ${CMAKE_SOURCE_DIR}/src/rs2.F
     ${CMAKE_SOURCE_DIR}/src/owi_ice.F
+    ${CMAKE_SOURCE_DIR}/src/owi_rain.F
     ${CMAKE_SOURCE_DIR}/src/itpackv.F
     ${CMAKE_SOURCE_DIR}/src/nodalattr.F
     ${CMAKE_SOURCE_DIR}/src/globalio.F

cmake/padcirc.cmake
@@ -16,6 +16,7 @@ if(BUILD_PADCIRC)
       ${CMAKE_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_SOURCE_DIR}/src/rs2.F
       ${CMAKE_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_SOURCE_DIR}/src/itpackv.F
       ${CMAKE_SOURCE_DIR}/src/nodalattr.F
       ${CMAKE_SOURCE_DIR}/src/globalio.F

cmake/padcswan.cmake
@@ -90,6 +90,7 @@ if(BUILD_PADCSWAN AND PERL_FOUND)
       ${CMAKE_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_SOURCE_DIR}/src/rs2.F
       ${CMAKE_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_SOURCE_DIR}/src/itpackv.F
       ${CMAKE_SOURCE_DIR}/src/nodalattr.F
       ${CMAKE_SOURCE_DIR}/src/globalio.F

adcirc/adcirc

cmake/adcirc.cmake
@@ -16,6 +16,7 @@ if(BUILD_ADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/rs2.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/itpackv.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/nodalattr.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/globalio.F

cmake/adcprep.cmake
@@ -15,6 +15,7 @@ if(BUILD_ADCPREP)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/rs2.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/wind.F
       ${CMAKE_CURRENT_SOURCE_DIR}/prep/presizes.F
       ${CMAKE_CURRENT_SOURCE_DIR}/prep/pre_global.F

cmake/adcswan.cmake
@@ -91,6 +91,7 @@ if(BUILD_ADCSWAN AND PERL_FOUND)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/rs2.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/itpackv.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/nodalattr.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/globalio.F

cmake/libadcirc.cmake
@@ -16,6 +16,7 @@ set(LIBADC_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/rs2.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/itpackv.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/nodalattr.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/globalio.F
@@ -40,6 +41,7 @@ set(LIBADC_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/src/sponge_layer.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/quadrature.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/couple2baroclinic3D.F)
+
 
 if(BUILD_LIBADCIRC_STATIC)
 

cmake/padcirc.cmake
@@ -18,6 +18,7 @@ if(BUILD_PADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/rs2.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/itpackv.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/nodalattr.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/globalio.F

cmake/padcswan.cmake
@@ -92,6 +92,7 @@ if(BUILD_PADCSWAN AND PERL_FOUND)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/rs2.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/itpackv.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/nodalattr.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/globalio.F