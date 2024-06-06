01b67d6be47a07305a6897fb1f1da906862e637b
Damrongsak Wirasaet <dwirasae@gmail.com>
2024-06-06 16:54:15 -0400

    Add modules for calculating the position of the Moon and Sun

--- cmake/adcswan.cmake
+++ cmake/adcswan.cmake
@@ -127,6 +127,12 @@ if(BUILD_ADCSWAN AND PERL_FOUND)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/couple2baroclinic3D.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/gl2loc_mapping.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/internaltide.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/astronomic.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/ephemerides.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/tidalpotential.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/sun.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/moon.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/sun_moon_system.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/couple2baroclinic3D.F)
 
