65db6b2e8d9335bfdb269bafe3f61a745da1e92e
Zach Cobell <zcobell@gmail.com>
2024-07-04 01:25:39 -0400

    Adding support for NVIDIA compilers
    NVIDIA compilers are evolved from the PGI compilers, and are
    showing up on more systems lately, so worth supporting
    
    Cleaning up compiler warnings in metis and swan

--- cmake/architecture.cmake
+++ cmake/architecture.cmake
@@ -25,7 +25,6 @@ if(${CMAKE_Fortran_COMPILER_ID} STREQUAL "GNU")
       "-ffixed-line-length-none"
       CACHE STRING "Compiler specific flag to enable extended Fortran line length")
 
-  # 64 bit array sizing
   if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
     set(Fortran_COMPILER_SPECIFIC_FLAG
         "-mcmodel=medium"
@@ -37,7 +36,7 @@ if(${CMAKE_Fortran_COMPILER_ID} STREQUAL "GNU")
   endif()
 
 elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL "Intel" OR ${CMAKE_Fortran_COMPILER_ID} STREQUAL "IntelLLVM")
-  # ifort
+  # ifort/ifx
   set(Fortran_LINELENGTH_FLAG
       "-132"
       CACHE STRING "Compiler specific flag to enable extended Fortran line length")
@@ -56,7 +55,6 @@ elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL "Intel" OR ${CMAKE_Fortran_COMPILER
     set(heaparray_FLAG "-heap-arrays ${STACKSIZE_TRIMMED}")
   endif("${STACKSIZE_TRIMMED}" STREQUAL "unlimited")
 
-  # 64 bit array sizing
   if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
     set(ifort_FLAG "${heaparray_FLAG} -assume byterecl -mcmodel=medium")
   else()
@@ -68,13 +66,12 @@ elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL "Intel" OR ${CMAKE_Fortran_COMPILER
       ${ifort_FLAG_TRIMMED}
       CACHE STRING "Compiler specific flags")
 
-elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL "PGI")
+elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL "PGI" OR ${CMAKE_Fortran_COMPILER_ID} STREQUAL "NVHPC")
   # pgf90
   set(Fortran_LINELENGTH_FLAG
       "-Mextend"
       CACHE STRING "Compiler specific flag to enable extended Fortran line length")
 
-  # 64 bit array sizing
   if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
     set(Fortran_COMPILER_SPECIFIC_FLAG
         "-Mlarge_arrays"
--- cmake/aswip.cmake
+++ cmake/aswip.cmake
@@ -28,13 +28,18 @@ if(BUILD_ASWIP)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/mesh.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/wind.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/netcdf_error.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F
       ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/KDTREE2/kdtree2.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
       ${CMAKE_CURRENT_SOURCE_DIR}/wind/vortex.F
       ${CMAKE_CURRENT_SOURCE_DIR}/wind/aswip.F)
+  
+  if(NETCDF_WORKING)
+    set(ASWIP_SOURCES ${ASWIP_SOURCES}
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/netcdf_error.F90
+    )
+  endif()
 
   add_executable(aswip ${ASWIP_SOURCES})
 
