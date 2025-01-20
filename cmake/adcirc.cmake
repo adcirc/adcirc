a922f975c84e8af63f60f8b1b860745cbc4a4bfc
Zach Cobell <zcobell@gmail.com>
2025-01-20 13:33:20 -0500

    Combining impl file

--- cmake/adcirc.cmake
+++ cmake/adcirc.cmake
@@ -62,8 +62,7 @@ if(BUILD_ADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/ephemerides.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/tidalpotential.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/boundaries/gwce_bc_forcing.F90
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/boundaries/gwce_bc_forcing_impl.F90)
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/boundaries/gwce_bc_forcing.F90)
 
   if(NETCDF_WORKING)
     set(ADCIRC_SOURCES
