69781bf698061930747420edd63e00c000b8dd73
Zach Cobell <zcobell@gmail.com>
2025-04-19 18:53:54 -0400

    Refactoring nws08 into its own module

--- cmake/adcirc.cmake
+++ cmake/adcirc.cmake
@@ -61,6 +61,7 @@ if(BUILD_ADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/sun_moon_system.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/ephemerides.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/tidalpotential.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/nws08.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F)
 
   if(NETCDF_WORKING)
