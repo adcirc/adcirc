8d7110e4e0f79471c4a9ab6f360ed813d3bf33c8
Zach Cobell <zcobell@gmail.com>
2025-09-25 15:39:49 -0400

    Converting netcdfio.F to netcdfio.F90 and fixing issues

--- cmake/libadcirc.cmake
+++ cmake/libadcirc.cmake
@@ -69,7 +69,7 @@ if(NETCDF_WORKING)
   set(LIBADC_SOURCES
       ${LIBADC_SOURCES}
       ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/netcdfio.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/netcdfio.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/netcdf_error.F90)
 endif()
 
