fc80041220ed23221b555aedf363d372272b2ac5
Zach Cobell <zcobell@gmail.com>
2025-10-15 13:55:23 -0400

    Updating vew1d.F -> vew1d.F90

--- cmake/adcirc.cmake
+++ cmake/adcirc.cmake
@@ -22,7 +22,7 @@ if(BUILD_ADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/global.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/boundaries.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/mesh.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/vew1d.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/vew1d.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/global_3dvs.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/harm.F
       ${CMAKE_CURRENT_SOURCE_DIR}/wind/vortex.F
--- cmake/padcirc.cmake
+++ cmake/padcirc.cmake
@@ -24,7 +24,7 @@ if(BUILD_PADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/global_3dvs.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/messenger.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/mesh.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/vew1d.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/vew1d.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/harm.F
       ${CMAKE_CURRENT_SOURCE_DIR}/wind/vortex.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/wind.F
