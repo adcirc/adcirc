72578603a7e885a72b607ab60da7020a839fd34f
Zach Cobell <zcobell@gmail.com>
2025-10-16 08:42:50 -0400

    Converting hashtable.F -> hashtable.F90

--- cmake/aswip.cmake
+++ cmake/aswip.cmake
@@ -21,7 +21,7 @@ if(BUILD_ASWIP)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/global.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/global_3dvs.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/boundaries.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/hashtable.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/hashtable.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/internaltide.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/nodalattr.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/mesh.F
