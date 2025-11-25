514700e3611c863445d633de6ee28b76c4dfae18
Chris Szpilka <cmszpilka@ou.edu>
2025-11-25 13:27:52 -0600

    Subject: Add new source term modules (rain/hydrology) to cmake. Details: Added lines to six cmake files to include new source terms.

--- cmake/padcirc.cmake
+++ cmake/padcirc.cmake
@@ -17,6 +17,7 @@ if(NOT BUILD_PADCIRC OR NOT MPI_FOUND)
   return()
 endif()
 
+<<<<<<< HEAD
 set(PADCIRC_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/src/sizes.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/constants.F90
@@ -38,6 +39,9 @@ set(PADCIRC_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/rs2.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/hydrology.F90
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/itpackv.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/itpackv.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/nodalattr.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/globalio.F
@@ -71,6 +75,70 @@ set(PADCIRC_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/src/moon.F90
     ${CMAKE_CURRENT_SOURCE_DIR}/src/sun_moon_system.F90
     ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F)
+=======
+  set(PADCIRC_SOURCES
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/sizes.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/constants.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/KDTREE2/kdtree2.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/global.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/boundaries.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/global_3dvs.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/messenger.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/mesh.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/vew1d.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/harm.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/wind/vortex.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/wind.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/nws08.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/hashtable.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/rs2.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_ice.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/rain.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/hydrology.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/itpackv.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/nodalattr.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/globalio.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/subdomain.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/gwce.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/wetdry.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/momentum.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/control.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/writer.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/write_output.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/couple2swan.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/adcirc.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/weir_boundary.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/normal_flow_boundary.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/read_input.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/cstart.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/hstart.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/timestep.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/vsmy.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/transport.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/driver.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/sponge_layer.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/quadrature.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/couple2baroclinic3D.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/gl2loc_mapping.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/internaltide.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/astronomic.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/ephemerides.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/tidalpotential.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/sun.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/moon.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/sun_moon_system.F90
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F)
+
+  if(NETCDF_WORKING)
+    set(PADCIRC_SOURCES
+        ${PADCIRC_SOURCES}
+        ${CMAKE_CURRENT_SOURCE_DIR}/src/owiwind_netcdf.F
+        ${CMAKE_CURRENT_SOURCE_DIR}/src/netcdfio.F90
+        ${CMAKE_CURRENT_SOURCE_DIR}/src/netcdf_error.F90)
+  endif()
+>>>>>>> Subject: Add new source term modules (rain/hydrology) to cmake.
 
 if(NETCDF_WORKING)
   set(PADCIRC_SOURCES
